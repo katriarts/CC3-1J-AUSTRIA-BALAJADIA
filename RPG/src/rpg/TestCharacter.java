1adc49f98000c53088ca62f04d387750cf5de65c
Kathy Austria <44886509+katriarts@users.noreply.github.com>
2019-01-31 14:35:00 +0800

    Update TestCharacter.java

--- RPG/src/rpg/TestCharacter.java
+++ RPG/src/rpg/TestCharacter.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package rpg;
 
-/**
- *
- * @author m304user
- */
 public class TestCharacter {
-    
+    public TestCharacter(){
+    Character kathy = new Character("Kathy", 5, 4, 3);
+        Character piolo = new Character("Piolo", 2, 1 ,6);
+        
+        for(int i = 1; i <= 100; i++){
+            System.out.println("ROUND " + i);
+            System.out.println(kathy.getName() + ": " + kathy.getCurrentLife());
+            System.out.println(piolo.getName() + ": " + piolo.getCurrentLife());
+            int kAttack = kathy.attack(), pAttack = piolo.attack();
+            System.out.println(kathy.getName() + " attacks " + piolo.getName() + " for " + kAttack);
+            System.out.println(piolo.getName() + " attacks " + kathy.getName() + " for " + pAttack);
+            piolo.wound(kAttack);
+            kathy.wound(pAttack);
+            System.out.println(" ");
+            if(kathy.getCurrentLife() <= 0){
+                System.out.println(piolo.getName() + " wins!");
+                break;
+            }
+            if (piolo.getCurrentLife() <= 0){
+                System.out.println(kathy.getName() + " wins!");
+                break;
+            }
+        }
+    }
 }
