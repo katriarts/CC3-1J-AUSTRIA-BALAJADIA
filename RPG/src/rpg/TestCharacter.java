a860e96514e3a22dd29c003283186338f551aeec
Kathy Austria <44886509+katriarts@users.noreply.github.com>
2019-01-31 14:45:21 +0800

    Update TestCharacter.java

--- RPG/src/rpg/TestCharacter.java
+++ RPG/src/rpg/TestCharacter.java
@@ -8,13 +8,26 @@ public class TestCharacter {
         
         for(int i = 1; i <= 100; i++){
             System.out.println("ROUND " + i);
+            
             System.out.println(kathy.getName() + ": " + kathy.getCurrentLife());
             System.out.println(piolo.getName() + ": " + piolo.getCurrentLife());
+            
             int kAttack = kathy.attack(), pAttack = piolo.attack();
+            
             System.out.println(kathy.getName() + " attacks " + piolo.getName() + " for " + kAttack);
             System.out.println(piolo.getName() + " attacks " + kathy.getName() + " for " + pAttack);
+            
             piolo.wound(kAttack);
             kathy.wound(pAttack);
+            
+            System.out.print("Kathy was healed for");
+            kathy.heal(1);
+            System.out.println(" ");
+            
+            System.out.print("Piolo was healed for");
+            piolo.heal(1);
+            System.out.println("\n");
+                       
             System.out.println(" ");
             if(kathy.getCurrentLife() <= 0){
                 System.out.println(piolo.getName() + " wins!");
