4beaaabff02264538d4cf329a3a200b5c7770ceb
Kathy Austria <44886509+katriarts@users.noreply.github.com>
2019-02-11 22:12:02 +0800

    Update CharacterDuel.java

--- RPG/src/rpg/CharacterDuel.java
+++ RPG/src/rpg/CharacterDuel.java
@@ -12,19 +12,6 @@ public class CharacterDuel {
             
            //UwU~
             System.out.println(" ");
-            
-           if(d.roll() < 3){
-               //w.castHeal();
-               System.out.println(w.getName() + " was healed for " + w.castHeal());
-           }
-           else if(d.roll() > 3){
-               //w.castLightningBolt();
-               System.out.println(w.getName() + " casts lightning bolt to " + m.getName());
-           }
-           else if(d.roll() == 3){
-               //w.attack();
-               System.out.println(w.getName() + " attacks " +  m.getName());
-           }
            
             int Battack = w.castLightningBolt();
             int Aattack = m.castBash();
@@ -36,35 +23,46 @@ public class CharacterDuel {
             w.wound(Battack);
             m.wound(Aattack);
             
+            if(d.roll() < 3){
+               //w.castHeal();
+               System.out.println(": " +w.getName() + " was healed for " + w.castHeal());
+           }
+           else if(d.roll() > 3){
+               //w.castLightningBolt();
+               System.out.println(w.getName() + " casts lightning bolt to " + m.getName());
+           }
+           else if(d.roll() == 3){
+               //w.attack();
+               System.out.println(w.getName() + " attacks " +  m.getName());
+           }
+            
             System.out.println(" ");
             System.out.println("HP of " + w.getName() + ": " + w.getCurrentLife());
             System.out.println("HP of " + m.getName() + ": " + m.getCurrentLife());
             System.out.println(" ");
+            
             if (w.getCurrentLife()<= 0 && m.getCurrentLife()>0){
-               /* System.out.println("HP of " + w.getName() + ": " + w.getCurrentLife());
-                System.out.println("HP of " + m.getName() + ": " + m.getCurrentLife());
-                System.out.println(" ");
-                */System.out.println(m.getName() + " WINS!!!");
+               System.out.println(m.getName() + " WINS!!!");
                 break;
             }
             else if (w.getCurrentLife() >= 0 && m.getCurrentLife()< 0){
-                /*System.out.println("HP of " + w.getName() + ": " + w.getCurrentLife());
-                System.out.println("HP of " + m.getName() + ": " + m.getCurrentLife());
-                System.out.println(" ");
-               */System.out.println(w.getName() + " WINS!!!");
+            System.out.println(w.getName() + " WINS!!!");
                 break;
             }
             else if (w.getCurrentLife()== 0 && m.getCurrentLife()== 0){                 
-                if (w.getCurrentLife() <= 0 && m.getCurrentLife() <= 0) {
-                    Math.max(0, w.getCurrentLife() );  
-                    Math.max(0, m.getCurrentLife() );
-                    /*System.out.println("HP of " + w.getName() + ": " + w.getCurrentLife());
-                    System.out.println("HP of " + m.getName() + ": " + m.getCurrentLife());
-                    System.out.println(" ");
-                    */System.out.println("IZ A TIE BOIII!!! :3");
+                    //Math.max(0, w.getCurrentLife() );  
+                    //Math.max(0, m.getCurrentLife() );
+                    System.out.println("IZ A TIE BOIII!!! :3");
+                    //System.exit(0);
                     break;
                     
-                }
+                
+            }
+            else if (Math.max(0, m.getCurrentLife()) <= 0)
+                if(Math.max(0, w.getCurrentLife()) <= 0){
+                System.out.println("IZ A TIE BOIII!!! :3");
+                //System.exit(0);
+                break;
             }
             
             
